0062705f64b1157534b2c658785c4a75d626d557
Vit Listik <tivvit@seznam.cz>
2018-06-14 13:53:50 +0200

    exmaples

---
+++ go/1-install.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+go get github.com/elastic/go-elasticsearch/client
---
+++ python/7-venv.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+pip3 install virtualenv
+virtualenv env
+source env/bin/activate
+ls env/lib/python3.6/site-packages/
